d5ac2b702a449f5adff5aa208fcb740bbc8af5d5
ryanjonathandev-pixel <ryanjonathan.dev@gmail.com>
2026-05-26 10:49:03 -0300

    Add files via upload

---
+++ AnaliZ.js
@@ -0,0 +1,45 @@
+function analisar() {
+    let idade = Number(document.getElementById("candidate1").value);
+let Option;
+
+if (idade < 16 && idade > 0) 
+    {
+   
+        Option = 1;
+} 
+
+else if (idade >= 16 && idade < 18)
+     {
+   
+        Option = 2;
+} 
+
+else if (idade >= 18 && idade < 70)
+     {
+    
+        Option = 3;
+} 
+
+else if (idade >= 70)
+     {
+   
+        Option = 4;
+}
+
+    switch (Option) {
+
+        case 1:
+            alert("Novo dms!");
+            break;
+        case 2:
+            alert("Você é apto á votar, mas não é obrigatório!");
+            document.createElement('form'); 
+            break;
+        case 3:
+            alert("Você é apto á votar, e é obrigatório!");
+            break;
+        case 4:
+            alert("Parabens! Você esta livre pra não se preocupar com o voto! FILHO DA PUTA");
+            break;
+    }
+}
