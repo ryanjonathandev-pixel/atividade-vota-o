0e4a2e2adbedb86259199611b9820aa4ad6f3a59
ryanjonathandev-pixel <ryanjonathan.dev@gmail.com>
2026-06-03 08:07:03 -0300

    Add files via upload

--- AnaliZ.js
+++ AnaliZ.js
@@ -1,49 +1,25 @@
 function analisar() {
-let idade = Number(document.getElementById("candidate1").value);
-let Option;
+let Option = document.getElementById("cidade").value;
 
-if (idade < 16 && idade > 0) 
-    {
-   
-        Option = 1;
-} 
-
-else if (idade >= 16 && idade < 18)
-     {
-   
-        Option = 2;
-} 
-
-else if (idade >= 18 && idade < 70)
-     {
-    
-        Option = 3;
-} 
-
-else if (idade >= 70 && idade < 150)
-     {
-   
-        Option = 4;
-}
-
-else {
-    alert("Idade inválida!");
-}
 
     switch (Option) {
 
-        case 1:
+        case "1":
             alert("Novo dms!");
             break;
-        case 2:
-            alert("Você é apto á votar, mas não é obrigatório!");
+        case "2":
+            let reposta = confirm("Você é apto á votar, mas não é obrigatório! deseja votar?");
+              if (resposta) {
+              window.open("escolha_seu_candidato.html");
+              else {
+              return 0;}
             window.open("escolha_seu_candidato.html");
             break;
-        case 3:
+        case "3":
             alert("Você é apto á votar, e é obrigatório!");
             window.open("escolha_seu_candidato.html");
             break;
-        case 4:
+        case "4":
             alert("Parabens! Você esta livre pra não se preocupar com o voto!");
             window.open("escolha_seu_candidato.html");
             break;
